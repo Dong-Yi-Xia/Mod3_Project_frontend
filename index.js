6ae23196dd02a19cea7f2b47f27ed4a8a2749b35
Dong-Yi-Xia <dong7777xia@gmail.com>
2020-09-28 21:40:05 -0400

    add total price

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Stable Element
 // _______________________________________________________________________
-
+let displayPrice = document.querySelector('div#displayPrice')
 let flavorList = document.querySelector('div.flavorList')
 let mainImage = document.querySelector('div.imageDisplay')
 let menuNarBar = document.querySelector('div.menuNarBar')
@@ -19,9 +19,10 @@ let purchaseButton = document.querySelector('button#purchaseButton')
 let reviewBox = document.querySelector('div#reviews')
 let reviewUL = document.querySelector('ul#reviewUL')
 
-let foundToppingObj = 0
-let foundMlikObj = 0
-let foundScoopObj = 0
+let foundToppingObj = {}
+let foundMlikObj = {}
+let foundScoopObj = {}
+let foundFlavorObj = {}
 
 
 
@@ -66,6 +67,8 @@ fetch('http://localhost:3000/flavors')
 // 
 // _______________________________________________________________________
 
+// menuNarBar.style.display = "none"
+
 function turnintoList(objFlavor) {
     let listUL = document.createElement('ul')
     let listLI = document.createElement('li')
@@ -77,6 +80,9 @@ function turnintoList(objFlavor) {
     changeToPointer(listLI)
     
     listLI.addEventListener("click", (evt)=>{
+        // menuNarBar.style.display = "block"
+        foundFlavorObj = objFlavor
+
         mainImage.innerText = ""
         mainImage.append(imageFl)
         imageFl.src = objFlavor.image
@@ -94,7 +100,7 @@ function turnintoList(objFlavor) {
 // ice cream options
 // _______________________________________________________________________
 
-function options(){  
+function options(element){  
     
     toppingDD.addEventListener("change", (evt)=>{
         currentInput = evt.target.value
@@ -119,13 +125,13 @@ function options(){
             let found = objArr.find(ele => {
                 return ele.name === currentInput
             }) 
-            foundMilkObj = found
+            foundMlikObj = found
         })
     })
 
     scoopDD.addEventListener("change", (evt)=>{
         currentInput = evt.target.value
-        selectedMilk.innerText = `Selected Scoop: ${currentInput}`
+        selectedScoop.innerText = `Selected Scoop: ${currentInput}`
 
         fetch('http://localhost:3000/scoops')
         .then(r => r.json())
@@ -149,27 +155,31 @@ options()
 function addToppingOption (objTopping) {
     let optionsTop = document.createElement('option')
         optionsTop.value = objTopping.name
-        optionsTop.innerText = objTopping.name
+        optionsTop.innerText = `$${objTopping.price} - ${objTopping.name}`
     toppingDD.append(optionsTop)
 }
 
 function addMilkOption (objMilk) {
     let optionsMilk = document.createElement('option')
         optionsMilk.value = objMilk.name
-        optionsMilk.innerText = objMilk.name
+        optionsMilk.innerText = `$${objMilk.price} - ${objMilk.name}`
     milkDD.append(optionsMilk)
 }
 
 function addScoopOption (objScoop) {
     let optionsScoop = document.createElement('option')
         optionsScoop.value = objScoop.number
-        optionsScoop.innerText = objScoop.number
+        optionsScoop.innerText = `$${objScoop.price} - ${objScoop.number}`
     scoopDD.append(optionsScoop)
 }
 
 function displayTotal(){
-    seeTotalButton.addEventListener("click", (evt)=>{
-        console.log(foundToppingObj,foundMilkObj,foundScoopObj)
+    seeTotalButton.addEventListener("click", (evt)=>{   
+        displayPrice.innerText = ''
+        let priceH2 = document.createElement('h2')
+            priceH2.innerText = `$ ${foundToppingObj.price +foundMlikObj.price + foundScoopObj.price + foundFlavorObj.price}`
+        displayPrice.append(priceH2)
+        console.log(foundToppingObj, foundMlikObj, foundScoopObj, foundFlavorObj)
     })
 }
 
@@ -181,4 +191,6 @@ function changeToPointer(li){
         li.style.cursor = "pointer"
     })
 }
+
+
 
