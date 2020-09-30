0084b0d5d0da7ed7d8997e8b955800b84fdd22cc
hortencia718 <hortenciaecisneros@gmail.com>
2020-09-30 16:05:55 -0400

    new branch

--- index.js
+++ index.js
@@ -15,23 +15,22 @@ let selectedScoop = document.querySelector('p#selectedScoop')
 let selectedPrice = document.querySelector('p#totalPrice')
 
 let seeTotalButton = document.querySelector('button#addButton')
-// let purchaseButton = document.querySelector('button#purchaseButton')
+let purchaseButton = document.querySelector('button#purchaseButton')
 let reviewBox = document.querySelector('div#reviews')
 let reviewUL = document.querySelector('ul#reviewUL')
 let submitReviewButton = document.querySelector('button#submitReviewButton')
 let reviewContent = document.querySelector('textarea#reviewContent')
 let formReview = document.querySelector('div#form-review')
 
+
 let foundToppingObj 
 let foundMilkObj 
 let foundScoopObj 
-let foundFlavorObj 
+let foundFlavorObj
 
 let foundUserObj = 1
 
-
-
-
+let mainBody = document.querySelector('div.main-body')
 
 
 // fetch request
@@ -82,9 +81,12 @@ function turnintoList(objFlavor) {
     flavorList.append(listUL)
     listUL.append(listLI)
     listLI.innerText = objFlavor.name
+
+    let heart = document.createElement('span')
+    heart.innerText = objFlavor.like ? "♥" :  "♡"
+    listUL.append(heart)
     
     changeToPointer(listLI)
-    
     listLI.addEventListener("click", (evt)=>{
         // menuNarBar.style.display = "block"
         foundFlavorObj = objFlavor
@@ -93,55 +95,106 @@ function turnintoList(objFlavor) {
         mainImage.append(imageFl)
         imageFl.src = objFlavor.image
         
-        reviewUL.innerText = ""
-        addingReviewObjtoHTML(objFlavor)
-
         formReview.innerText = ""
-        createReviewForm()
+        createReviewForm(objFlavor)
 
-       
+        reviewUL.innerText = ""
+        objFlavor.reviews.forEach(reviewObj => {
+            addingReviewObjtoHTML(reviewObj)
+        })     
 
         displayTotal()
 
-        
     })
+
+    changeToPointer(heart)
+    heart.addEventListener("click", (evt)=>{
+        heartTurnOnOff(objFlavor, heart)
+    })
+
 }
 
-// ice cream options
+
+// heart like methods 
 // _______________________________________________________________________
+let heartTurnOnOff = (objFlavor, heart) =>{
+    let redheart = !objFlavor.like
+    fetch(`http://localhost:3000/flavors/${objFlavor.id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            like: redheart
+        })  
+    })
+    .then(r => r.json())
+    .then(objFlavorUpdate => {
+        objFlavor.like = objFlavorUpdate.like
+        heart.innerText = objFlavorUpdate.like ? "♥" :  "♡"
+    })
 
-let addingReviewObjtoHTML = (obj) =>{
-    obj.reviews.forEach(reviewObj => {
+}
+
+
+// let purchaseNow = () => {
+//     purchaseButton.addEventListener("click", (evt)=>{
+//         console.log("asdasdadasa")
+//     })
+// }
+
+
+// review helper methods 
+// _______________________________________________________________________
+let addingReviewObjtoHTML = (reviewObj) => {
         let reviewLI = document.createElement('li')
-        reviewLI.innerText = reviewObj.review
-        reviewUL.append(reviewLI)
+            reviewLI.innerText = reviewObj.review
+        let reviewDeleteButton = document.createElement('button')
+            reviewDeleteButton.innerText = "Remove Review"
+        reviewLI.append(reviewDeleteButton)
+        reviewUL.prepend(reviewLI)
+
+        deleteAReview(reviewDeleteButton, reviewObj, reviewLI)   
+    }
+
+
+let deleteAReview = (reviewDeleteButton, reviewObj, reviewLI) =>{
+    reviewDeleteButton.addEventListener("click", (evt)=>{
+        fetch(`http://localhost:3000/reviews/${reviewObj.id}`, {
+            method: "DELETE"
+        })
+        .then(r => r.json())
+        .then(response => {
+            //update the frontend
+           reviewLI.remove()
+        })
     })
 }
 
-let createReviewForm = () =>{
+
+let createReviewForm = (objFlavor) =>{
     let formLabel = document.createElement('label')
-        formLabel.innerText = "Creating a New Review"
+        formLabel.innerText = "Create a Reviews"
     let formTextArea = document.createElement('textarea')
         formTextArea.id = "reviewContent"
         formTextArea.type = "text"
+        formTextArea.placeholder = "Please First, Select a FLAVOR, a TOPPING, a MILK base, and the Number of SCOOPS"
+        formTextArea.rows = "5"
+        formTextArea.cols = "40"
     let formButton = document.createElement('button')
         formButton.type = "submit"
         formButton.id = "submitReviewButton"
         formButton.innerText = "Submit Review"
     formReview.append(formLabel,formTextArea,formButton)    
 
-    submitAReview(formButton, formTextArea)
+    submitAReview(formButton, formTextArea, objFlavor)
 }
 
-let submitAReview = (formButton, formTextArea) => {
+let submitAReview = (formButton, formTextArea, objFlavor) => {
     formButton.addEventListener("click", (evt)=>{
         evt.preventDefault()
 
         let content = formTextArea.value
-    //    console.log(foundMilkObj)
-    //    console.log(foundToppingObj)
-    //    console.log(foundScoopObj)
-    //    console.log(foundFlavorObj)
         fetch('http://localhost:3000/reviews', {
             method: "POST",
             headers: {
@@ -149,7 +202,7 @@ let submitAReview = (formButton, formTextArea) => {
             },
             body: JSON.stringify({
                 //need to create userObj 
-                user_id: foundFlavorObj,
+                user_id: foundUserObj,
                 flavor_id: foundFlavorObj.id,
                 topping_id: foundToppingObj.id,
                 milk_id: foundMilkObj.id,
@@ -158,15 +211,22 @@ let submitAReview = (formButton, formTextArea) => {
             })
         })
         .then(r => r.json())
-        .then(createReview => {
-            console.log(createReview)
+        .then(createReviewObj => {
+            //update the frontend
+            addingReviewObjtoHTML(createReviewObj)
+            //Update Inner Memory
+            objFlavor.reviews.push(createReviewObj)
+
+            formReview.innerText = ""
+            createReviewForm(objFlavor)
         })
 
     })
 }
 
 
-
+// ice cream options
+// _______________________________________________________________________
 function options(){  
     
     toppingDD.addEventListener("change", (evt)=>{
@@ -213,12 +273,14 @@ function options(){
         })
     })
 
-
     displayTotal()
+
 }
 
 
 options()
+
+
 
 // ice cream options display
 // _______________________________________________________________________
@@ -229,6 +291,8 @@ function addToppingOption (objTopping) {
         optionsTop.innerText = `$${objTopping.price} - ${objTopping.name}`
     toppingDD.append(optionsTop)
 }
+
+
 
 function addMilkOption (objMilk) {
     let optionsMilk = document.createElement('option')
@@ -248,9 +312,42 @@ function displayTotal(){
     // seeTotalButton.addEventListener("click", (evt)=>{   
         displayPrice.innerText = ''
         let priceH2 = document.createElement('h2')
-            priceH2.innerText = `Price: $${foundToppingObj.price +foundMilkObj.price + foundScoopObj.price + foundFlavorObj.price}`
+            priceH2.innerText = `Total Price: $${foundToppingObj.price +foundMilkObj.price + foundScoopObj.price + foundFlavorObj.price}`
         displayPrice.append(priceH2)
+
+        purchaseNow()
     // })
+}
+
+function purchaseNow(){
+    purchaseButton.addEventListener("click", (evt)=>{ 
+        fetch('http://localhost:3000/purchased_logs', {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                //need to create userObj 
+                user_id: foundUserObj,
+                flavor_id: foundFlavorObj.id,
+                topping_id: foundToppingObj.id,
+                milk_id: foundMilkObj.id,
+                scoop_id: foundScoopObj.id,
+                price: foundToppingObj.price +foundMilkObj.price + foundScoopObj.price + foundFlavorObj.price
+            })
+        })
+        .then(r => r.json())
+        .then(createPurchasedObj => {
+            mainBody.innerText = ""
+            let thankYouH1 = document.createElement('h1')
+                thankYouH1.innerText = `Thank You For Your Purchase!!! See You Again. 
+                Flavor: ${foundFlavorObj.name}
+                Topping: ${foundToppingObj.name} 
+                Milk Base: ${foundMilkObj.name}  
+                # of Scoops: ${foundScoopObj.number}` 
+            mainBody.append(thankYouH1)
+        })
+    })
 }
 
 // extra
